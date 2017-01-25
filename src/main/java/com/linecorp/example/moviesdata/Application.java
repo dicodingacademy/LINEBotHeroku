e5816531158c6602ad6530c1d78041c924e123e2
Nur Rohman <ennurr31@gmail.com>
2017-01-26 01:05:44 +0700

    app again

--- src/main/java/com/linecorp/example/moviesdata/Application.java
+++ src/main/java/com/linecorp/example/moviesdata/Application.java
@@ -3,20 +3,20 @@ package com.linecorp.example.moviesdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer
-{
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder app)
-    {
-        return app.sources(LineBotController.class);
+public class Application {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 
-    public static void main(String [] args)
-    {
-        SpringApplication.run(LineBotController.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
