0c44f7d63a17aaf762b7b0675e6f7db55ec7a07f
Nur Rohman <ennurr31@gmail.com>
2017-01-27 16:56:22 +0700

    update

--- src/main/java/com/linecorp/example/moviesdata/Event.java
+++ src/main/java/com/linecorp/example/moviesdata/Event.java
@@ -5,9 +5,12 @@ import java.util.List;
 public class Event {
 
     private Boolean success;
+    private List<Data> data;
     public Boolean getSuccess ()
     {
         return success;
     }
-
+    public List<Data> getData(){
+        return data;
+    }
 }
