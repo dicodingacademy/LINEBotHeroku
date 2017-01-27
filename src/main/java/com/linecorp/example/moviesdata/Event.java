44bde69e569000ec19b27dd1fc8730b8cb54a184
Nur Rohman <ennurr31@gmail.com>
2017-01-27 14:11:32 +0700

    update

--- src/main/java/com/linecorp/example/moviesdata/Event.java
+++ src/main/java/com/linecorp/example/moviesdata/Event.java
@@ -2,33 +2,33 @@ package com.linecorp.example.moviesdata;
 
 public class Event {
 
-    private EventData[] data;
+//    private EventData[] data;
 
     private Boolean success;
 
-    public EventData[] getData ()
-    {
-        return data;
-    }
-
-    public void setData (EventData[] data)
-    {
-        this.data = data;
-    }
+//    public EventData[] getData ()
+//    {
+//        return data;
+//    }
+//
+//    public void setData (EventData[] data)
+//    {
+//        this.data = data;
+//    }
 
     public Boolean getSuccess ()
     {
         return success;
     }
 
-    public void setSuccess (Boolean success)
-    {
-        this.success = success;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "ClassPojo [data = "+data+", success = "+success+"]";
-    }
+//    public void setSuccess (Boolean success)
+//    {
+//        this.success = success;
+//    }
+//
+//    @Override
+//    public String toString()
+//    {
+//        return "ClassPojo [data = "+data+", success = "+success+"]";
+//    }
 }
